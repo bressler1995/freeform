20b55fe1f4a7ee515b692fae6fa8d71082bef475
carlofabyss <carlofabyss@hotmail.com>
2020-03-25 15:13:24 -0700

    getting somewhere

--- js/sketch.js
+++ js/sketch.js
@@ -1,32 +1,36 @@
+//Credit to The Code Train for the awesome algorithm
+
 let cols;
 let rows;
 let current; // = new float[cols][rows];
 let previous; // = new float[cols][rows];
 let reds = 255;
+let greens = 0;
 let blues = 0;
+let mode = "red";
 
 let dampening = 0.999;
 
 function setup() {
+  angleMode(DEGREES);
   pixelDensity(1);
   createCanvas(windowWidth, windowHeight);
   cols = width;
   rows = height;
-  // The following line initializes a 2D cols-by-rows array with zeroes
-  // in every array cell, and is equivalent to this Processing line:
-  // current = new float[cols][rows];
   current = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
   previous = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
   frameRate(60);
 }
 
 function mouseDragged() {
-  previous[mouseX][mouseY] = random(500, 1000);
+  previous[mouseX][mouseY] = random(500, 1500);
 }
 
 function draw() {
+
   background(0);
   loadPixels();
+
   for (let i = 1; i < cols - 1; i++) {
     for (let j = 1; j < rows - 1; j++) {
       current[i][j] =
@@ -53,7 +57,40 @@ function draw() {
   previous = current;
   current = temp;
 
-  fill(255, 0, 0, 20);
+  if(mode == "red") {
+    if(reds > 20) {
+        reds -= 0.1;
+    }
+    if(blues < 220) {
+        blues += 0.1;
+    }
+
+    if(greens < 150) {
+        greens += 0.5;
+    }
+
+    if(reds <= 20 && blues >= 220 && greens >= 150) {
+        mode = "blue";
+    }
+
+  } else if(mode == "blue") {
+    if(reds < 200) {
+        reds += 0.1;
+    }
+    if(blues > 50) {
+        blues -= 0.1;
+    }
+
+    if(greens > 50) {
+        greens -= 0.5;
+    }
+
+    if(reds >= 200 && blues <= 50 && greens <= 50) {
+        mode = "red";
+    }
+  }
+
+  fill(reds, greens, blues, 160);
   rect(0, 0, windowWidth, windowHeight);
 }
 
