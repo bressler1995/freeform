b8871b58df5d00173c9936a5a50f09526bf47d4c
carlofabyss <carlofabyss@hotmail.com>
2020-03-25 13:23:35 -0700

    add ripples

--- js/sketch.js
+++ js/sketch.js
@@ -1,37 +1,60 @@
-let mycanvas;
-let ellipserecord = [];
-let mysize = 20;
-let clear_opt, save_opt;
+// 2D Water Ripples
+// The Coding Train / Daniel Shiffman
+// https://thecodingtrain.com/CodingChallenges/102-2d-water-ripple.html
+// https://youtu.be/BZUdGqeOD0w
+// https://editor.p5js.org/codingtrain/sketches/tYXtzNSl
 
-function preload() {
+// Algorithm: https://web.archive.org/web/20160418004149/http://freespace.virgin.net/hugo.elias/graphics/x_water.htm
 
-}
+let cols;
+let rows;
+let current; // = new float[cols][rows];
+let previous; // = new float[cols][rows];
+
+let dampening = 0.999;
 
 function setup() {
-    mycanvas = createCanvas(400, 400);
-    clear_opt = select('#clear_opt');
-    clear_opt.mousePressed(clearEverything);
-    save_opt = select('#save_opt');
-    save_opt.mousePressed(saveEverything)
-}
-
-function draw() {
-    background(255);
-    fill(0);
-    for(i=0; i<ellipserecord.length; i++) {
-        ellipse(ellipserecord[i].ellipsex, ellipserecord[i].ellipsey, ellipserecord[i].ellipsesize, ellipserecord[i].ellipsesize);
-    }
+  pixelDensity(1);
+  createCanvas(600, 400);
+  cols = width;
+  rows = height;
+  // The following line initializes a 2D cols-by-rows array with zeroes
+  // in every array cell, and is equivalent to this Processing line:
+  // current = new float[cols][rows];
+  current = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
+  previous = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
 }
 
 function mouseDragged() {
-    console.log("X: " + mouseX + ", Y: " + mouseY);
-    ellipserecord.push({ellipsex:mouseX, ellipsey:mouseY, ellipsesize:20});
+  previous[mouseX][mouseY] = 500;
 }
 
-function clearEverything() {
-    ellipserecord = [];
-}
+function draw() {
+  background(0);
 
-function saveEverything() {
-    saveCanvas(mycanvas, 'myWorkOfArt', 'jpg');
+  loadPixels();
+  for (let i = 1; i < cols - 1; i++) {
+    for (let j = 1; j < rows - 1; j++) {
+      current[i][j] =
+        (previous[i - 1][j] +
+          previous[i + 1][j] +
+          previous[i][j - 1] +
+          previous[i][j + 1]) /
+          2 -
+        current[i][j];
+      current[i][j] = current[i][j] * dampening;
+      // Unlike in Processing, the pixels array in p5.js has 4 entries
+      // for each pixel, so we have to multiply the index by 4 and then
+      // set the entries for each color component separately.
+      let index = (i + j * cols) * 4;
+      pixels[index + 0] = current[i][j];
+      pixels[index + 1] = current[i][j];
+      pixels[index + 2] = current[i][j];
+    }
+  }
+  updatePixels();
+
+  let temp = previous;
+  previous = current;
+  current = temp;
 }
